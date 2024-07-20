a7518f35209fa2a129e20307bfb62c0208502df2
vdmkhrp <vdmkhrp@gmail.com>
2024-07-20 22:43:01 +0300

    responsive section catalog

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
 const btn = document.querySelector(".burger-btn");
 const menu = document.querySelector(".menu");
 const overlay = document.querySelector(".overlay");
+const ms = document.querySelector(".menu-sidebar");
+const sidebar = document.querySelector(".sidebar");
+
 btn.addEventListener("click", () => {
-  btn.classList.toggle("active");
-  menu.classList.toggle("active");
-  overlay.classList.toggle("active");
+  btn.classList.add("active");
+  menu.classList.add("active");
+  overlay.classList.add("active");
 });
+
 overlay.addEventListener("click", () => {
   btn.classList.remove("active");
   menu.classList.remove("active");
+  ms.classList.remove("active");
+  sidebar.classList.remove("active");
   overlay.classList.remove("active");
+});
+
+ms.addEventListener("click", () => {
+  ms.classList.add("active");
+  sidebar.classList.add("active");
+  overlay.classList.add("active");
 });
